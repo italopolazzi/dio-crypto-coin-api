8ca3a6bf3905662ed8ce21d3e81f21d3e9744faf
Ítalo Polazzi Ferreira <itpzzi@gmail.com>
2020-07-25 20:17:01 -0300

    add: PWA sw.js file and event listeners

---
+++ sw.js
@@ -0,0 +1,15 @@
+const self = this
+
+
+self.addEventListener('install', event => {
+  console.log(event);
+
+})
+self.addEventListener('fetch', event => {
+  console.log(event);
+
+})
+self.addEventListener('activate', event => {
+  console.log(event);
+
+})
